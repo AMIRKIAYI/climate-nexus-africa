c17b76ab47cbd0799586beb795d4ead56b7b3131
AMIRKIAYI <amirjuma036@gmail.com>
2026-03-18 09:46:20 +0300

    stopped some link pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Home from './pages/Home';
-import Team from './pages/Team';
-import VisionMission from './pages/VisionMission';
-import Impact from './pages/Impact';
-import Training from './pages/Training';
-import Community from './pages/Community';
-import Contact from './pages/Contact';
+// import Team from './pages/Team';
+// import VisionMission from './pages/VisionMission';
+// import Impact from './pages/Impact';
+// import Training from './pages/Training';
+// import Community from './pages/Community';
+// import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -17,12 +17,12 @@ function App() {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/team" element={<Team />} />
+            {/* <Route path="/team" element={<Team />} />
             <Route path="/vision-mission" element={<VisionMission />} />
             <Route path="/impact" element={<Impact />} />
             <Route path="/training" element={<Training />} />
             <Route path="/community" element={<Community />} />
-            <Route path="/contact" element={<Contact />} />
+            <Route path="/contact" element={<Contact />} /> */}
           </Routes>
         </main>
         <Footer />
